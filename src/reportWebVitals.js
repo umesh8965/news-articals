2ecdebc475c9de156c1a840363e547188dc7bdb9
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-26 07:46:05 +0000

    edit_file
    # Path: step_1/for_each/edit_file
    # UUID: ac750e2f-04d6-4751-bdd5-4a4aae5796d4
    
    # Changed files:
    src/reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,6 @@
-const reportWebVitals = onPerfEntry => {
+type PerfEntryHandler = (entry: PerformanceEntry) => void;
+
+const reportWebVitals = (onPerfEntry?: PerfEntryHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
